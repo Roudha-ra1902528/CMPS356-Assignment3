bd023726a48d4e167bfe9cafa19fffa0b4975b46
Roudha-ra1902528 <97511320+Roudha-ra1902528@users.noreply.github.com>
2022-12-19 13:20:31 +0300

    removed

--- assignment03/word-search/app/page.jsx
+++ assignment03/word-search/app/page.jsx
@@ -1,13 +1,7 @@
-'use client';
-import { useEffect } from 'react'
-import styles from './page.module.css'
-import { useRouter } from 'next/navigation';
 
 
 export default function Home() {
-  const router = useRouter()
   
-  useEffect(()=>{router.push(`/words?query=`)},[])
 
   return (
     <div className={styles.container}>
